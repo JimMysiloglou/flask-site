0f73689c66bac759d1db57cfce04e3a67fce82fd
Mysiloglou Dimitrios <misijimmys@gmail.com>
2022-06-16 22:05:58 +0300

    custom error handlers

--- webapp/main/controllers.py
+++ webapp/main/controllers.py
@@ -55,3 +55,19 @@ def contact():
         return redirect(url_for("main.contact"))
 
     return render_template("contact.html", form=form)
+
+@main_blueprint.app_errorhandler(403)
+def forbidden(e):
+    return render_template('errors/403.html'), 403
+
+@main_blueprint.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+@main_blueprint.app_errorhandler(415)
+def unsupported_media_type(e):
+    return render_template('errors/415.html'), 415
+
+@main_blueprint.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
